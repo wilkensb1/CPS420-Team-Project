de953f79492762f3ba03dde9652c25aab3476936
lanea17 <lanea17@udayton.edu>
2023-12-06 19:48:52 -0500

    initial set up

--- my_env/Scripts/activate.bat
+++ my_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Boob\Documents\GitHub\CPS-490-Repo-1\CPS420-Team-Project\my_env
+set VIRTUAL_ENV=C:\Users\Owner\OneDrive\Documents\GitHub\CPS420-Team-Project\my_env
 
 if not defined PROMPT set PROMPT=$P$G
 
